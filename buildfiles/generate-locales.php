71a5aea59ea9ec2f421485c53222fc082e70571a
Katharina Bogad <katharina@hacked.xyz>
2021-07-02 19:43:30 +0200

    Fixed missing constant in build system

--- buildfiles/generate-locales.php
+++ buildfiles/generate-locales.php
@@ -4,23 +4,24 @@ use Grocy\Services\LocalizationService;
 /* This file statically generates json to handle
  * frontend translations.
  */
-define("GROCY_DATAPATH", __DIR__ . '/../data');
+const REPO_BASE = __DIR__ . '/..';
+define("GROCY_DATAPATH", REPO_BASE .'/data');
 
 // Load composer dependencies
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once REPO_BASE .'/vendor/autoload.php';
 // Load config files
 if(file_exists(GROCY_DATAPATH . '/config.php')) {
 	require_once GROCY_DATAPATH . '/config.php';
 }
-require_once __DIR__ . '/../php/config-dist.php'; // For not in own config defined values we use the default ones
+require_once REPO_BASE .'/php/config-dist.php'; // For not in own config defined values we use the default ones
 
-echo "Searching for localizations in " . __DIR__ . '/../localization/* \n';
+echo "Searching for localizations in " . REPO_BASE .'/localization/* \n';
 
-$translations = array_filter(glob(__DIR__ . '/../localization/*'), 'is_dir');
+$translations = array_filter(glob(REPO_BASE .'/localization/*'), 'is_dir');
 
 // ensure the target directory is there
-if(!is_dir(__DIR__ . '/../public/js/locales/grocy/')) {
-	mkdir(__DIR__ . '/../public/js/locales/grocy/', 0777, true);
+if(!is_dir(REPO_BASE .'/public/js/locales/grocy/')) {
+	mkdir(REPO_BASE .'/public/js/locales/grocy/', 0777, true);
 }
 
 foreach($translations as $lang) {
@@ -28,5 +29,5 @@ foreach($translations as $lang) {
 	echo "Generating " . $culture . "...\n";
 	$ls = LocalizationService::getInstance($culture, true);
 	$ls->LoadLocalizations(false);
-	file_put_contents(__DIR__ .'/../public/js/locales/grocy/'.$culture.'.json', $ls->GetPoAsJsonString());
+	file_put_contents(REPO_BASE .'/public/js/locales/grocy/'.$culture.'.json', $ls->GetPoAsJsonString());
 }
