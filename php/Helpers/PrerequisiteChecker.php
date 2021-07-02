9a2ad1b9495d3404df09a5b4f42a5e68f3ed951c
Katharina Bogad <katharina@hacked.xyz>
2021-07-02 17:30:25 +0200

    Fixed ErrorMessage in PrerequisiteChecker

--- php/Helpers/PrerequisiteChecker.php
+++ php/Helpers/PrerequisiteChecker.php
@@ -38,7 +38,7 @@ class PrerequisiteChecker
 	{
 		if (!file_exists(GROCY_DATAPATH . '/config.php'))
 		{
-			throw new ERequirementNotMet('config.php in data directory (' . GROCY_DATAPATH . ') not found. Have you copied config-dist.php to the data directory and renamed it to config.php?');
+			throw new ERequirementNotMet('config.php in data directory (' . GROCY_DATAPATH . ') not found. Have you copied php/config-dist.php to the data directory and renamed it to config.php?');
 		}
 	}
 
