cb57a49e1c9c8e974aabc0d246e4918c3cbe34f4
Katharina Bogad <katharina@hacked.xyz>
2021-07-02 23:27:36 +0200

    Remove build artifact

--- js/pages/Stock/Overview.vue?vue&type=template&id=31afdab6&lang.js
+++
@@ -1,6 +0,0 @@
-import { openBlock as _openBlock, createBlock as _createBlock } from "vue";
-
-export function render(_ctx, _cache, $props, $setup, $data, $options) 
-{
-	return (_openBlock(), _createBlock("p", null, " Hi! "));
-}
